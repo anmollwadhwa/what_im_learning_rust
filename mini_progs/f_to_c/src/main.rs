34a30f4b18350c6e3b1d9b82a1ab66ddadd0e5a9
anmollwadhwa <172998376+anmollwadhwa@users.noreply.github.com>
2026-09-24 19:39:38 +0530

    Added loop and continue statement

--- mini_progs/f_to_c/src/main.rs
+++ mini_progs/f_to_c/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     let mut user_temp = String::new();
     let mut user_unit =String::new();
     println!("Welcome to temperature converter");
+
+ loop {    
     println!("Enter the temp ( without unit ) you wish to convert");
 
     io::stdin()
@@ -22,7 +24,7 @@ fn main() {
         Ok(n) => n,
         Err(_e) => {
                 println!("Please enter a valid number");
-                return
+                continue;
 
         }
 
@@ -38,13 +40,15 @@ fn main() {
 
         let converted = c_to_f(user_temp);
         println!("The converted temperature is {converted}F")
-        
+
     } else {
+
         println!("Please enter C or F unit");
     }
 
-}
+  }
 
+}
 fn f_to_c (user_temp : f64) ->  f64 {
         let converted = (user_temp - 32.0) * (5.0/9.0);
         converted
